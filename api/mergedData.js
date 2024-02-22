1ecf87b4bdb92fef7c1a94aa73a448053366ee0f
taylorseager <taylorseager718@gmail.com>
2024-02-21 21:15:30 -0600

    book details finished

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,17 @@
 // for merged promises
+
+import { getSingleBook } from './bookData';
+import { getSingleAuthor } from './authorData';
+
+// TODO: Get data for viewBook
+const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  // GET SINGLE BOOK
+  getSingleBook(firebaseKey).then((bookObject) => { // returns single book object
+    getSingleAuthor(bookObject.author_id) // we nest this promise so that we can use the book object
+      .then((authorObject) => resolve({ ...bookObject, authorObject }));
+  }).catch(reject);
+  // GET AUTHOR
+  // Create an object that has book data and an object named authorObject
+});
+
+export default getBookDetails;
