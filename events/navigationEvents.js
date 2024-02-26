1df24973740c0e9d9f24de657e791400cdc20ac3
taylorseager <taylorseager718@gmail.com>
2024-02-25 23:01:37 -0600

    no error messages

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -31,11 +31,13 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
+    console.warn('clicked authors');
     getAuthors().then(showAuthors);
   });
 
   // TODO: Filter Fave authors
   document.querySelector('#fav-authors').addEventListener('click', () => {
+    console.warn('clicked fave authors');
     showFaveAuthors().then(showAuthors);
   });
 
--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,5 +1,5 @@
 const clearDom = () => {
-  document.querySelector('#store').innerHTML = '';
+  document.querySelector('#search-store').innerHTML = '';
   document.querySelector('#add-button').innerHTML = '';
   document.querySelector('#form-container').innerHTML = '';
   document.querySelector('#view').innerHTML = '';
